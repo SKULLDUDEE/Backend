e9262fa5e8cbdbc5c014a4f04efda62ea2301504
RAJAT <rajat@RAJATs-MacBook-Air.local>
2025-04-01 16:08:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import { configDotenv } from 'dotenv';
+// import express from 'express';
+// import connectDB from './middleware/db.js';
+
+// configDotenv();
+
+// const { PORT, FRONTEND_PORT, RENDER_FRONTED_PORT, DEV_FRONTEND_PORT } = process.env;
+// const SERVER_PORT = PORT || 3000;
+// const app = express();
+// await connectDB();
+
+// // Parser for Parsing JSON and Cookie
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(
+//     cors({
+//         origin: [FRONTEND_PORT, RENDER_FRONTED_PORT, DEV_FRONTEND_PORT],
+//         credentials: true,
+//     })
+// );
+
+// import errorMiddleware from "./middleware/errorMiddleware.js";
+// import authRoutes from "./routes/auth.js";
+// import eventRoutes from './routes/events.js';
+
+// app.use("/auth", authRoutes);
+// app.use('/event', eventRoutes);
+
+// // Error Middleware
+// app.use(errorMiddleware);
+
+
+// // Connect to MongoDB and start the server
+// connectDB()
+//     .then(() => app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`)))
+//     .catch((err) => {
+//         console.error("Failed to connect to MongoDB:", err.message);
+//         process.exit(1);
+//     });
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { configDotenv } from 'dotenv';
@@ -16,8 +58,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: [FRONTEND_PORT, RENDER_FRONTED_PORT, DEV_FRONTEND_PORT],
+        origin: [
+            'https://singular-douhua-9f92d5.netlify.app',  
+            'http://localhost:3000',                      
+            
+        ],
         credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Explicitly allow OPTIONS
     })
 );
 
